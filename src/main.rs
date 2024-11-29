411b9e8124acba16eb9dde8fa9a981f4f8d67bb0
Aaron Geiger <aaron@geigr.dev>
2024-11-29 01:42:06 +0100

    Rework

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,85 @@
-use std::{fs, path::PathBuf};
+use std::{fs, path::PathBuf, process::ExitCode};
 
-use clap::Parser;
-use image::{imageops, DynamicImage, GenericImageView};
+use clap::{Args, Parser};
+use image::{GenericImageView, RgbImage};
+use thiserror::Error;
 
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[clap(author, version)]
-struct Args {
-    input: PathBuf,
-
-    #[clap(short, long, default_value = "output.png")]
-    output: PathBuf,
-
-    #[clap(long)]
-    identity: Option<PathBuf>,
-
-    #[clap(long, short)]
-    max: Option<usize>,
+pub enum Cli {
+    Stitch(StitchArgs),
+    Compare(CompareArgs),
 }
 
-fn main() -> std::io::Result<()> {
-    let args = Args::parse();
-    let mut files = fs::read_dir(&args.input)
-        .expect("Could not read dir")
-        .map(|entry| entry.expect("Could not read dir"))
-        .filter(|e| e.file_type().expect("Could not get File type").is_file())
-        .map(|e| e.path())
-        .collect::<Vec<PathBuf>>();
+#[derive(Args)]
+pub struct StitchArgs {}
 
-    alphanumeric_sort::sort_path_slice(&mut files);
+#[derive(Args)]
+pub struct CompareArgs {
+    a: PathBuf,
+    b: PathBuf,
+    output: Option<PathBuf>,
 
-    let identity = image::open(match args.identity {
-        Some(path) => path,
-        None => files.get(0).unwrap().clone(),
-    })
-    .unwrap();
+    #[arg(short, long)]
+    name: Option<String>,
+}
 
-    let chunk_size = args.max.unwrap_or(files.len());
-    let chunks = files.chunks(chunk_size);
+#[derive(Error, Debug)]
+pub enum CliError {
+    #[error("IO Error {0}")]
+    IoError(#[from] std::io::Error),
 
-    let mut blocks = Vec::new();
-    for chunk in chunks {
-        let mut block =
-            DynamicImage::new_rgb8(identity.width() * chunk.len() as u32, identity.height());
+    #[error("Image Error {0}")]
+    ImageError(#[from] image::error::ImageError),
 
-        for (i, path) in chunk.iter().enumerate() {
-            let img = image::open(path).unwrap();
-            imageops::overlay(&mut block, &img, i as u32 * identity.width(), 0);
-        }
-        blocks.push(block);
+    #[error("Input images have different dimensions")]
+    DifferentDimensions,
+}
+
+fn main() -> impl std::process::Termination {
+    if let Err(err) = match Cli::parse() {
+        Cli::Stitch(_) => panic!("Not implemented"),
+        Cli::Compare(compare_args) => compare(&compare_args),
+    } {
+        eprintln!("{err}");
+        return ExitCode::FAILURE;
     }
 
-    let mut image = DynamicImage::new_rgb8(
-        identity.width() * chunk_size as u32,
-        identity.height() * blocks.len() as u32,
+    return ExitCode::SUCCESS;
+}
+
+fn compare(args: &CompareArgs) -> Result<(), CliError> {
+    let a = image::open(&args.a)?;
+    let b = image::open(&args.b)?;
+
+    if a.dimensions() != b.dimensions() {
+        return Err(CliError::DifferentDimensions);
+    }
+
+    let mut output = RgbImage::new(a.width(), a.height());
+    let mut diff_pixels = 0usize;
+
+    for ((a, b), out) in a.pixels().zip(b.pixels()).zip(output.pixels_mut()) {
+        let a = a.2 .0;
+        let b = b.2 .0;
+
+        let diff = a != b;
+        diff_pixels += diff as usize;
+        out.0 = [0xff, 0xff * !diff as u8, 0xff * !diff as u8];
+    }
+
+    if let Some(name) = &args.name {
+        print!("{name}: ");
+    }
+
+    println!(
+        "Imgages have {diff_pixels} different pixels. ({}%)",
+        diff_pixels as f32 / (output.width() * output.height()) as f32 * 100f32
     );
 
-    for (i, block) in blocks.iter().enumerate() {
-        imageops::overlay(&mut image, block, 0, i as u32 * identity.height());
+    if let Some(output_path) = &args.output {
+        output.save(output_path)?;
     }
-
-    image.save(args.output).expect("Could not save image");
 
     Ok(())
 }
